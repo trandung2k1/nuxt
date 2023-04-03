e2ab945c5fcb4d7e5049e69362a45ec7236bb307
dunghenry <trandungksnb6930082001>
2023-04-03 21:35:17 +0700

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header',
+};
+</script>
+<style scoped>
+ul {
+    display: flex;
+    list-style: none;
+}
+a {
+    text-decoration: none;
+    font-size: 20px;
+    padding: 0 10px;
+    color: blue;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Header></Header>
+        <Nuxt />
+        <Footer></Footer>
+    </div>
+</template>
+<script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+export default {};
+</script>
+<style></style>
